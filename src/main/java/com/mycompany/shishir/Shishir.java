23a116f96e1cc3cae252ef21261a2497be67ed24
sheikh-shishir <ahmed2305101594@diu.edu.bd>
2024-10-30 21:17:34 +0600

    first commit

---
+++ src/main/java/com/mycompany/shishir/Shishir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shishir;
+
+/**
+ *
+ * @author macbookair
+ */
+public class Shishir {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Shishir");
+    }
+}
